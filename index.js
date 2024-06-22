6ec71099a2008d3870d3cd861c2bf2ba9412fa1c
Rishabh <rishabhbhatia02@gmail.com>
2024-06-23 00:05:33 +0530

    index

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 // file storage
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "https://connect-c6ou.onrender.com/public/assets");
+        cb(null, "public/assets");
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname);
